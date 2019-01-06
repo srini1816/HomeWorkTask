3afa0177e2628f8205f013e44e2ca4253ea92955
srini1816 <srinivas.che2009@gmail.com>
2019-01-06 22:18:44 +0000

    add all

---
+++ src/MathsOperations/ForLoop.java
@@ -0,0 +1,10 @@
+package MathsOperations;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 50; i >= 0; --i) {
+            System.out.println( i);
+        }
+    }
+}
---
+++ src/MathsOperations/MathsWork.java
@@ -0,0 +1,19 @@
+package MathsOperations;
+
+public class MathsWork {
+    public static void main(String[] args) {
+        int A,B,C;
+        A=125;
+        B=24;
+        C=A+B;
+        System.out.println(C);
+        C=A-B;
+        System.out.println(C);
+        C=A/B;
+        System.out.println(C);
+        C=A%B;
+        System.out.println(C);
+
+
+    }
+}
